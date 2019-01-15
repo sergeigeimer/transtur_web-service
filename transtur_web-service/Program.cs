0746bebe79ad92ff512eda24cc5208b2d5e4bab3
sergeigeimer <sergeigeimerchat@gmail.com>
2019-01-15 21:13:27 +0500

    Добавьте файлы проекта.

---
+++ transtur_web-service/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace transtur_web_service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
