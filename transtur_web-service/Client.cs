f4d92014175b774179116e112485b215071d33ab
Sergey A. Chernyaev <chernyaev_sergei@mail.ru>
2019-01-24 14:52:00 +0500

    404 Error)

---
+++ transtur_web-service/Client.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace transtur_web_service
+{
+    class Client
+    {
+        private void SendError(TcpClient Client, int Code)
+        {
+            string CodeStr = Code.ToString() + " " + ((HttpStatusCode)Code).ToString();
+            string Html = "<html><body><h1>" + CodeStr + "</h1></body></html>";
+            string Str = "HTTP/1.1 " + CodeStr + "\nContent-type: text/html\nContent-Length:" + Html.Length.ToString() + "\n\n" + Html;
+            byte[] Buffer = Encoding.ASCII.GetBytes(Str);
+            Client.GetStream().Write(Buffer, 0, Buffer.Length);
+            Client.Close();
+        }
+        public Client(TcpClient Client)
+        {
+            string Request = "";
+            byte[] Buffer = new byte[1024];
+            int Count;
+            while ((Count = Client.GetStream().Read(Buffer, 0, Buffer.Length)) > 0)
+            {
+                Request += Encoding.ASCII.GetString(Buffer, 0, Count);
+                if (Request.IndexOf("\r\n\r\n") >= 0 || Request.Length > 4096)
+                {
+                    break;
+                }
+            }
+            Match ReqMatch = Regex.Match(Request, @"^\w+\s+([^\s\?]+)[^\s]*\s+HTTP/.*|");
+            if (ReqMatch == Match.Empty)
+            {
+                SendError(Client, 400);
+                return;
+            }
+            string RequestUri = ReqMatch.Groups[1].Value;
+            RequestUri = Uri.UnescapeDataString(RequestUri);
+            if (RequestUri.IndexOf("..") >= 0)
+            {
+                SendError(Client, 400);
+                return;
+            }
+            if (RequestUri.EndsWith("/"))
+            {
+                RequestUri += "index.html";
+            }
+            string FilePath = "www/" + RequestUri;
+            if (!File.Exists(FilePath))
+            {
+                SendError(Client, 404);
+                return;
+            }
+            string Extension = RequestUri.Substring(RequestUri.LastIndexOf('.'));
+            string ContentType = "";
+            switch (Extension)
+            {
+                case ".htm":
+                case ".html":
+                    ContentType = "text/html";
+                    break;
+                case ".css":
+                    ContentType = "text/stylesheet";
+                    break;
+                case ".js":
+                    ContentType = "text/javascript";
+                    break;
+                case ".jpg":
+                    ContentType = "image/jpeg";
+                    break;
+                case ".jpeg":
+                case ".png":
+                case ".gif":
+                    ContentType = "image/" + Extension.Substring(1);
+                    break;
+                default:
+                    if (Extension.Length > 1)
+                    {
+                        ContentType = "application/" + Extension.Substring(1);
+                    }
+                    else
+                    {
+                        ContentType = "application/unknown";
+                    }
+                    break;
+            }
+            FileStream FS;
+            try
+            {
+                FS = new FileStream(FilePath, FileMode.Open, FileAccess.Read, FileShare.Read);
+            }
+            catch (Exception)
+            {
+                SendError(Client, 500);
+                return;
+            }
+            string Headers = "HTTP/1.1 200 OK\nContent-Type: " + ContentType + "\nContent-Length: " + FS.Length + "\n\n";
+            byte[] HeadersBuffer = Encoding.ASCII.GetBytes(Headers);
+            Client.GetStream().Write(HeadersBuffer, 0, HeadersBuffer.Length);
+            while (FS.Position < FS.Length)
+            {
+                Count = FS.Read(Buffer, 0, Buffer.Length);
+                Client.GetStream().Write(Buffer, 0, Count);
+            }
+
+            FS.Close();
+            Client.Close();
+        }
+    }
+}
--- transtur_web-service/Server.cs
+++ transtur_web-service/Server.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 
 namespace transtur_web_service
 {
-    class Service
+    class Server
     {
+        TcpListener Listener;
+        public Server(int Port)
+        {
+            Listener = new TcpListener(IPAddress.Any, Port);
+            Listener.Start();
+            while (true)
+            {
+                ThreadPool.QueueUserWorkItem(new WaitCallback(ClientThread), Listener.AcceptTcpClient());
+            }
+        }
+
+        static void ClientThread(Object StateInfo)
+        {
+            new Client((TcpClient)StateInfo);
+        }
+
+        ~Server()
+        {
+            if (Listener != null)
+            {
+                Listener.Stop();
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("TransTur Web-service load");
+            int MaxThreadsCount = Environment.ProcessorCount * 4;
+            ThreadPool.SetMaxThreads(MaxThreadsCount, MaxThreadsCount);
+            ThreadPool.SetMinThreads(2, 2);
+            new Server(80);
         }
     }
 }
